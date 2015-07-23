ce6757a4e60647c814b949a20b1eb69bd475440d
Juan Pablo Rinaldi <juampi@juampi.com>
2015-07-22 22:48:38 -0300

    Update RSVP form recipients

--- app/mailers/rsvp_form.rb
+++ app/mailers/rsvp_form.rb
@@ -7,9 +7,10 @@ class RSVPForm < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "RSVP Form",
-      :to => "juampi@juampi.com",
-      :from => "André and Ceci <info@andreandceci.com>"
+      subject: "RSVP Form",
+      to: "Cecilia Rinaldi <cecirinaldi@gmail.com>, André Thiollier <andrethiollier@hotmail.com>, Belén Cesa Martínez <belen@barbaradiez.com>, Laura Castro <laura@barbaradiez.com>",
+      bcc: "Juan Pablo Rinaldi <juampi@juampi.com>",
+      from: "André and Ceci <info@andreandceci.com>"
     }
   end
 end
